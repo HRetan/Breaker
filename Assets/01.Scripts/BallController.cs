bc2f61ae2b787a6a54d84dfd06617aa2d71d5983
kairencer <kairencer@gmail.com>
2018-12-06 15:41:32 +0900

    Start

---
+++ Assets/01.Scripts/BallController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum BALLSTATE
+{
+    START,
+    MOVE,
+    TOUCH,
+    GRAP,
+    DEAD
+}
+
+public class BallController : MonoBehaviour {
+  
+    private float fSpeed = 0f;
+    private Transform trans;
+    private BALLSTATE state;
+
+    private GameObject BarObject;
+    private Transform BarTrans;
+    private Vector3 vector;
+
+    private bool isFire = false;
+    
+    // Use this for initialization
+	void Start () {
+        BarObject = GameObject.FindGameObjectWithTag("Player");
+        BarTrans = BarObject.GetComponent<Transform>();
+        trans = GetComponent<Transform>();
+        state = BALLSTATE.START;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(state)
+        {
+            case BALLSTATE.START:
+                StartBall();
+                break;
+            case BALLSTATE.MOVE:
+                break;
+            case BALLSTATE.TOUCH:
+                break;
+            case BALLSTATE.GRAP:
+                break;
+            case BALLSTATE.DEAD:
+                break;
+        }
+    }
+
+    void StartBall()
+    {
+        vector.Set(BarTrans.position.x, BarTrans.position.y + 0.25f, BarTrans.position.z);
+        trans.position = vector;
+
+        if (isFire)
+            state = BALLSTATE.MOVE;
+    }
+
+    void Move()
+    {
+
+    }
+}
---
+++ Assets/01.Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float fSpeed;
+
+    private Vector3 vector;
+    private Transform trans;
+
+    bool isRight = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        trans = gameObject.GetComponent<Transform>();
+        Debug.Log("호출");
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            if (vector.x != 0)
+            {
+                transform.Translate(vector.x * fSpeed, 0, 0);
+            }
+            else if (vector.y != 0)
+            {
+                transform.Translate(0, vector.y * fSpeed, 0);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Wall")
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+                if (vector.x != 0)
+                {
+                    transform.Translate(-vector.x * fSpeed, 0, 0);
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * fSpeed, 0);
+                }
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+
+    }
+}
